7731b495204e420f68681822aa11b1a6fd60e188
Nathan Wise <nathanwise@gmail.com>
2011-06-05 19:50:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twss::Application.config.secret_token = '3a12ce25d653649acf3648517205bf7567765c54e21adc80ebd2f2bc8d3c4b8706e0d05e7950eeded181fcb7a5845bba42a514c95212e01793b1d90c8d5deada'
